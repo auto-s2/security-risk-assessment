a0d7deaedd22634ba78f055ad6e71ba9c757ffe5
Marco <marco.ehrlich@th-owl.de>
2024-01-23 09:32:32 +0100

    Naming of knowledge base clarified and fixed

--- src/setup.py
+++ src/setup.py
@@ -23,7 +23,7 @@ def initialize():
     PRINT_RESULTS = True
     MACHINE_ID_SHORT = "CPS"
 
-    EXCEL_AUTOS2_INFORMATION_BASE_PATH = BASE_PATH + "/knowledge/autos2-knowledge_2023_04_06.xlsx"
+    EXCEL_AUTOS2_INFORMATION_BASE_PATH = BASE_PATH + "/knowledge/autos2-knowledge_2024_01_23.xlsx"
     TAB_ICS_ATTACK_INTEL_TAL_MAPPING = "ICS_ATT&CK-Intel_TAL-Mapping"
     TAB_MITIGATION_IEC_62443_MAPPING = "Mitigations-IEC_62443-Mapping"
     TAB_CVE_ICS_MAPPING = "CVE-ICS_ATT&CK-Mapping"
